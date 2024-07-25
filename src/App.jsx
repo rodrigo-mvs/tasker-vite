5d8ba0be8ae774cba98edf36b52af3c5e22ba7de
rodrigo-mvs <rodrigo.mvs05@gmail.com>
2024-07-25 11:06:52 +0100

    scnd commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ToDoList from './ToDoList'
+import ReactDOM from 'react-dom/client'
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <ToDoList />
+  </React.StrictMode>,
+)
---
+++ src/ToDoList.jsx
@@ -0,0 +1,106 @@
+import React , { useState , useEffect } from "react";
+import "./ToDoList.css";
+import { FaPaperPlane } from "react-icons/fa";
+import { FaTrash } from "react-icons/fa";
+import { FaCheck } from "react-icons/fa";
+import { FaUndoAlt } from "react-icons/fa";
+import { FaTrashAlt } from "react-icons/fa";
+import RokiRoki from "./assets/notasks.png"; 
+
+
+
+function ToDoList() {
+
+    const listaStorage = localStorage.getItem("taskList");
+
+    const [taskList, setTaskList] = useState(listaStorage ? JSON.parse(listaStorage) : []);
+    const [newTask, setNewTask] = useState("");
+    const [currentDate, setCurrentDate] = useState(getDate());
+
+    useEffect(() => {
+        localStorage.setItem("taskList", JSON.stringify(taskList));
+    }, [taskList]);
+
+
+    function addEvent(form) {
+        form.preventDefault();
+        if (newTask === "") return;
+        console.log(newTask + currentDate);
+        setNewTask(newTask);
+        setTaskList([...taskList, {text: '[' + currentDate + '] ➜ ' + newTask, done: false}]);
+        setNewTask("");
+        document.getElementById("prompt").focus();
+    }
+
+
+    function getDate() {
+        const today = new Date();
+        const month = today.getMonth() + 1;
+        const year = today.getFullYear();
+        const date = today.getDate();
+        return `${date}-${month}-${year}`;
+      }
+
+
+    function inverter(index) {
+        const listaAuxiliar = [...taskList];
+        listaAuxiliar[index].done = !listaAuxiliar[index].done;
+        setTaskList(listaAuxiliar);
+    }
+
+    function eliminar(index) {
+        const listaAuxiliar = [...taskList];
+        listaAuxiliar.splice(index, 1);
+        setTaskList(listaAuxiliar);
+    }
+
+    function delAll() {
+        setTaskList([]);
+    }
+
+
+    return (
+        <div>
+            <h1>Tasker</h1>
+
+            <form id="prompt" onSubmit={addEvent}>
+                <input type="text" value={newTask} onChange={(e) => setNewTask(e.target.value)} placeholder="Enter a task" />
+                <button className="add" type="submit"><FaPaperPlane /></button>
+                <button className="delall" type="button" onClick={() => { delAll() }}><FaTrashAlt /></button>
+            </form>
+            
+
+            
+            <div className="lista">
+                { taskList.length < 1 ? <img id="noTasks" src={RokiRoki}/> :
+
+                taskList.map((task, index) => (
+
+                    <div key={index} className={task.done ? "item done" : "item"}>
+                        <span>{task.text}</span>
+
+
+                        {task.done
+                        ?
+                        <form className="buttons">
+                            <button className="redo" type="button" onClick={() => { inverter(index) }}> <FaUndoAlt /></button>
+                            <button className="delete-done" type="button" onClick={() => { eliminar(index) }}> <FaTrash /></button>
+                        </form>
+                        :
+                        <form className="buttons">
+                            <button className="tick" type="button" onClick={() => { inverter(index) }}> <FaCheck /></button>
+                            <button className="delete" type="button" onClick={() => { eliminar(index) }}> <FaTrash /></button>
+                        </form>}
+
+                    </div>))}
+
+
+            </div>
+
+        </div>
+    );
+
+}
+
+
+export default ToDoList;
